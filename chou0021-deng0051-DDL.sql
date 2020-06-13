9f7069608a5a0a445779b87a4b1d8f4c6b722c49
kevluo <kevin.luo@outlook.com>
2020-06-13 18:31:25 -0400

    add sql files

---
+++ chou0021-deng0051-DDL.sql
@@ -0,0 +1,96 @@
+﻿-- FileName: chou0021-deng0051-DDL.sql
+-- Date    : June 11 2020
+-- Author  : Harpal Choudhary & Feiqiong Deng
+-- Comment : Assignment1-InventoryII-Tables with Constraints
+
+DROP VIEW IF EXISTS Cust_CAN_CHN_IND;
+DROP MATERIALIZED VIEW IF EXISTS Prod_Rus;
+
+DROP TABLE IF EXISTS Invoice_Line_T;
+DROP TABLE IF EXISTS Product_T;
+DROP TABLE IF EXISTS Invoice_T;
+DROP TABLE IF EXISTS Customer_T;
+DROP TABLE IF EXISTS City_T;
+DROP TABLE IF EXISTS Country_T;
+
+-- create country table
+CREATE TABLE Country_T(
+   Cntry_Code         CHAR(3),
+   Cntry_Name         VARCHAR(30),
+   Cntry_Population   BIGINT DEFAULT NULL,
+   CONSTRAINT Cntry_Code_PK PRIMARY KEY(Cntry_Code)
+);
+
+-- create city table
+CREATE TABLE City_T(
+   City_ID            INT,
+   City_Name          VARCHAR(30),
+   Cntry_Code         CHAR(3),
+   City_Population    BIGINT DEFAULT NULL,
+   CONSTRAINT City_ID_PK PRIMARY KEY(City_ID),
+   CONSTRAINT Cntry_Code_FK FOREIGN KEY(Cntry_Code) REFERENCES Country_T(Cntry_Code)  
+);
+
+-- create customer table
+CREATE TABLE Customer_T(
+   cust_id        character(4) NOT NULL,
+   cust_fname     character varying(30),
+   cust_lname     character varying(30) NOT NULL,
+   cust_phone     character varying(15) NOT NULL,
+   cust_address   character varying(20) NOT NULL,
+   cust_city      character varying(15) NOT NULL,
+   cust_prov      character(15),
+   cust_postcode  character(6) NOT NULL,
+   cust_balance   numeric(9,2),
+   cust_country   CHAR(3),  
+   CONSTRAINT pk_customer PRIMARY KEY (cust_id),
+   CONSTRAINT fk_cust_country FOREIGN KEY (cust_country) REFERENCES Country_T(Cntry_Code)  
+);
+
+-- create invoice table
+CREATE TABLE Invoice_T(
+   invoice_number   character(6) NOT NULL,
+   cust_id          character(4) NOT NULL,
+   invoice_date     date DEFAULT now(),
+   CONSTRAINT pk_invoice PRIMARY KEY (invoice_number),
+   CONSTRAINT fk_customer FOREIGN KEY(cust_id) REFERENCES Customer_T(cust_id)
+);
+
+-- create product table
+CREATE TABLE Product_T(
+   prod_code         character(5) NOT NULL,
+   prod_description  character varying(60) NOT NULL,
+   prod_indate       date NOT NULL DEFAULT now(),
+   prod_qoh          integer NOT NULL,
+   prod_min          integer,
+   prod_price        numeric(5,2) NOT NULL,
+   prod_discount     integer,
+   cntry_origin      CHAR(3),
+   CONSTRAINT pk_product PRIMARY KEY (prod_code),
+   CONSTRAINT fk_cntry_origin FOREIGN KEY (cntry_origin) REFERENCES Country_T (Cntry_Code)
+);
+
+-- create invoice_line table
+CREATE TABLE Invoice_Line_T(
+   invoice_number    character(6) NOT NULL,
+   invoice_line      integer NOT NULL,
+   prod_code         character(5) NOT NULL,
+   line_unit         integer NOT NULL,
+   line_price        numeric(9,2) NOT NULL,
+   CONSTRAINT pk_invoice_line PRIMARY KEY (invoice_number,invoice_line),
+   CONSTRAINT fk_product FOREIGN KEY (prod_code) REFERENCES Product_T(prod_code)
+);
+
+-- create a dynamic view named as "Cust_CAN_CHN_IND" which  display all the customers from Canada, China and India.
+
+CREATE VIEW Cust_CAN_CHN_IND AS
+  select Cust_fname, cust_lname, cust_city, cust_country from customer_t 
+  where cust_country in ('CAN', 'CHN', 'IND') order by cust_country;
+
+-- create a materialized view named as Prod_Rus that display all the prouducts made in Russia
+
+CREATE MATERIALIZED VIEW Prod_Rus AS 
+  Select Prod_Description, Prod_Code, Cntry_Origin from Product_t
+  where Cntry_Origin = 'RUS';
+
+-- eof: InventoryII-DDL.sql
---
+++ chou0021-deng0051-QUERY.sql
@@ -0,0 +1,32 @@
+﻿-- FileName: chou0021-deng0051-query.sql
+-- Date    : June 11 2020
+-- Author  : Harpal Choudhary & Feiqiong Deng
+-- Comment : Assignment1-InventoryII-Tables with Constraints
+
+select * from country_t;
+select * from customer_t;
+select * from city_t;
+select * from product_t;
+select * from invoice_t;
+select * from invoice_line_t;
+
+-- display data from cut_can_chn_Ind view, all customers from Canada, China and India
+select * from cust_can_chn_ind;
+select * from cust_can_chn_ind where cust_country = 'CAN';
+
+-- refresh after running DML file or inserting data in product table.
+refresh materialized view Prod_rus;
+
+-- display data from prod_rus materialized  view displays all products mmade in Russia
+
+select * from Prod_Rus;
+
+-- Join Statements left join customer table to country table and displays all the countries which doesnt have any customers 
+select country_t.cntry_name, cust_fname, cust_lname from country_t
+left join customer_t
+on customer_t.cust_country = country_t.cntry_code where customer_t.cust_country is null;
+
+-- right join displays all the countries where products are bought.
+select country_t.cntry_name, prod_description from country_t
+right join product_t
+on product_t.cntry_origin = country_t.cntry_code;
